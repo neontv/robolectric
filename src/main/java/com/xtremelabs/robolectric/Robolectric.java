57ae901e61945673e8493ed78135346eaa1c2b32
Aaron VonderHaar <pair+avonderhaar@pivotallabs.com>
2012-11-20 15:11:19 -0800

    Add ShadowAppWidgetHost#getContext()

--- src/main/java/com/xtremelabs/robolectric/Robolectric.java
+++ src/main/java/com/xtremelabs/robolectric/Robolectric.java
@@ -2,6 +2,7 @@ package com.xtremelabs.robolectric;
 
 import android.accounts.AccountManager;
 import android.app.*;
+import android.appwidget.AppWidgetHost;
 import android.appwidget.AppWidgetHostView;
 import android.appwidget.AppWidgetManager;
 import android.bluetooth.BluetoothAdapter;
@@ -148,6 +149,7 @@ public class Robolectric {
                 ShadowAnimationSet.class,
                 ShadowAnimationUtils.class,
                 ShadowApplication.class,
+                ShadowAppWidgetHost.class,
                 ShadowAppWidgetHostView.class,
                 ShadowAppWidgetManager.class,
                 ShadowArrayAdapter.class,
@@ -477,6 +479,10 @@ public class Robolectric {
 
     public static ShadowApplication shadowOf(Application instance) {
         return (ShadowApplication) shadowOf_(instance);
+    }
+
+    public static ShadowAppWidgetHost shadowOf(AppWidgetHost instance) {
+        return (ShadowAppWidgetHost) shadowOf_(instance);
     }
 
     public static ShadowAppWidgetHostView shadowOf(AppWidgetHostView instance) {
